47090b5a32a5fd209863ce4ed0926f6c643a132b
Krystian Manczak <kryman0@gmail.com>
2020-06-23 18:46:11 +0200

    2: Frontend
    Added logic for inserting a new user into db on the backend side for the route #/register.

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,9 +1,15 @@
 <template>
-    <div>
+    <div class="form-layout">
         <form v-on:submit.prevent="postForm">
-            <input name="text" type="text" v-model="text" />
-            <input type="submit" value="Register" />
-            <input type="button" value="Clear form" v-on:click="clearForm" />
+            <fieldset>
+                <legend>Register</legend>
+                    <label for="email">E-mail</label>
+                    <input id="email" name="email" type="email" v-model="email" />
+                    <label for="password">Password</label>
+                    <input id="password" name="password" type="password" v-model="password" />
+                    <input type="submit" value="Register" />
+            </fieldset>
+
         </form>
     </div>
 </template>
@@ -14,39 +20,62 @@ import utils from "../models/utils";
 export default {
     data: function () {
         return {
-            text: null,
+            email: null,
+            password: null,
         };
     },
-    // updated: function () {
-    //     this.$nextTick(function () {
-    //         this.postForm();
-    //     });
-    // },
     methods: {
         postForm: function () {
-            console.log(this.text);
-            // let form = document.getElementById("registerForm");
-            // console.log(form);
+            let request = new Request(
+                utils.localhostFullUrl() + "/register", {
+                method: "POST",
+                headers: {
+                    // "Content-Type": "application/x-www-form-urlencoded",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email: this.email,
+                    password: this.password,
+                }),
+            });
 
-            const data = { text: "hellos" };
-            // let request = new Request(
-            //     utils.localhostFullUrl() + "/register", {
-            //     method: "POST",
-            //     headers: {
-            //         "Content-Type": "application/json",
-            //     },
-            //     // body: new FormData(document.getElementById("registerForm")),
-            //     body: JSON.stringify(data),
-            // });
+            fetch(request)
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(utils.messages.error, err));
 
-            // fetch(request)
-            // .then(response => response.json())
-            // .then(data => console.log(data))
-            // .catch(err => console.log(utils.messages.error, err));
+            this.clearForm();
         },
         clearForm: function () {
-            this.text = null;
-        }
+            this.email = null;
+            this.password = null;
+        },
     },
 }
+// Todos:
+// Kör clearForm i slutet av postForm och skicka en indikation till vyn att det blivit postat.
 </script>
+
+<style scoped>
+    .form-layout {
+        width: 500px;
+        margin: 50px 0;
+    }
+    input {
+        display: block;
+        width: 150px;
+        margin: 5px 0;
+        padding: 5px;
+    }
+
+    input[type="submit"] {
+        width: 164px;
+        margin-top: 20px;
+        cursor: pointer;
+    }
+
+    label {
+        display: block;
+        margin: 10px 0 0;
+    }
+</style>
