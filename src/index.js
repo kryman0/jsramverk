5c7ca7e143786c916f9c2ad52f2e3a529a92c744
Krystian Manczak <kryman0@gmail.com>
2020-07-28 16:07:43 +0200

    3: devops. Added webpack-merge for merging common file from dev or production mode. Deploying to live production server is now working. Have not yet tested db on the live server.

--- src/index.js
+++ src/index.js
@@ -14,28 +14,33 @@ import Login from "./views/Login.vue";
 import Register from "./views/Register.vue";
 import Reports from "./views/reports/Reports.vue";
 import ReportsDetails from "./views/reports/Details.vue";
-// import TopNavbar from "./views/TopNavbar.vue";
 
 
-Vue.use(VueRouter);
+function component() {
+    const element = document.createElement("div");
+    element.id = "app";
+
+    document.body.appendChild(element);
 
 
-const router = new VueRouter({
-    routes: [
-        { path: "/", component: Home },
-        { path: "/register", component: Register },
-        { path: "/login", component: Login },
-        { path: "/reports", component: Reports },
-        { path: "/reports/week/:id", component: ReportsDetails },
-    ],
-});
+    Vue.use(VueRouter);
 
 
-new Vue({
-    router,
-    render: v => v(App),
-    // components: {
-    //     // TopNavbar // Shorthand for "top-navbar": TopNavbar
-    //     App // Shorthand for "top-navbar": TopNavbar
-    // }
-}).$mount("#app");
+    const router = new VueRouter({
+        routes: [
+            { path: "/", component: Home },
+            { path: "/register", component: Register },
+            { path: "/login", component: Login },
+            { path: "/reports", component: Reports },
+            { path: "/reports/week/:id", component: ReportsDetails },
+        ],
+    });
+
+
+    new Vue({
+        router,
+        render: v => v(App),        
+    }).$mount("#app");
+}
+
+component();
---
+++ webpack.common.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: "./src/index.js",
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    resolve: {
+        alias: {
+            vue$: path.resolve(__dirname, "./node_modules/vue/dist/vue.esm.js"),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: "vue-loader",
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "vue-style-loader",
+                    "css-loader",
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Production',
+        }),
+    ],
+};
